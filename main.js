ddeae0c45e2cc76077ff584cb6d59ffb9a7d5678
laxman1186 <laxman1186@gmail.com>
2020-06-13 18:49:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,98 @@
+
+const url = require("url");
+const path = require("path");
+const {app, BrowserWindow, Menu, protocol, ipcMain} = require('electron');
+const log = require('electron-log');
+const {autoUpdater} = require("electron-updater");
+
+let mainWindow
+
+let template = []
+if (process.platform === 'darwin') {
+  // OS X
+  const name = app.getName();
+  template.unshift({
+    label: name,
+    submenu: [
+      {
+        label: 'About ' + name,
+        role: 'about'
+      },
+      {
+        label: 'Quit',
+        accelerator: 'Command+Q',
+        click() { app.quit(); }
+      },
+    ]
+  })
+}
+function sendStatusToWindow(text) {
+  log.info(text);
+  mainWindow.webContents.send('message', text);
+}
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      webSecurity: false
+    }
+   
+  })
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+  //mainWindow.loadURL("http://localhost:4200");
+  mainWindow.loadURL(`file://${__dirname}/version.html#v${app.getVersion()}`);
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools()
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+})
+autoUpdater.on('update-available', (info) => {
+  sendStatusToWindow('Update available.');
+})
+autoUpdater.on('update-not-available', (info) => {
+  sendStatusToWindow('Update not available.');
+})
+autoUpdater.on('error', (err) => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+})
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+  sendStatusToWindow('Update downloaded');
+});
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow()
+})
+
+function openModal(){
+  const { BrowserWindow } = require('electron');
+  let modal = new BrowserWindow({ parent: mainWindow, modal: true, show: false })
+  modal.loadURL('https://www.sitepoint.com')
+  modal.once('ready-to-show', () => {
+    modal.show()
+  })
+}
+
+ipcMain.on('openModal', (event, arg) => {
+  openModal()
+})
+
